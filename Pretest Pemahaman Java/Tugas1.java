0f3e6e3d31a17356d2e4a7a4eae07185e2275bf9
faidnawa <faidnawa@gmail.com>
2017-02-27 11:15:37 +0700

    Created by Coding Ground Client

---
+++ Pretest Pemahaman Java/Tugas1.java
@@ -0,0 +1,8 @@
+public class Tugas1{
+
+     public static void main(String []args){
+        String nama = "Faidl";
+        String kota = "Jepara";
+        System.out.println(nama + " " + kota);
+     }
+}
