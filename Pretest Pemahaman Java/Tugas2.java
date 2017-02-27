c6e25f1c7677c3c2e669ea4f313636e03645e471
faidnawa <faidnawa@gmail.com>
2017-02-27 11:15:39 +0700

    Created by Coding Ground Client

---
+++ Pretest Pemahaman Java/Tugas2.java
@@ -0,0 +1,10 @@
+public class Tugas2{
+
+     public static void main(String []args){
+        String[] charArray ={ "faidl", "fahruddin", "nawa" }; 
+        
+        for (int i = 0; i < charArray.length; i++) {
+         System.out.print(charArray[i] + " ");
+        }
+     }
+}
